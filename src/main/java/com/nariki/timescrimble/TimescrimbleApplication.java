b82411acb2fea62eb9ae662535a5a4e535df5bbc
hocinemediani <dolcejeannarciss@gmail.com>
2026-04-01 08:14:25 +0200

    first commit

---
+++ src/main/java/com/nariki/timescrimble/TimescrimbleApplication.java
@@ -0,0 +1,13 @@
+package com.nariki.timescrimble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimescrimbleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimescrimbleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nariki/timescrimble/TimescrimbleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nariki.timescrimble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimescrimbleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
