8bd3c2e5eee22f7bc02eb35ed9c15da7b2ef94d7
hocinemediani <dolcejeannarciss@gmail.com>
2026-04-01 08:16:39 +0200

    changed package name

--- src/main/java/com/narbaniki/timescrimble/TimescrimbleApplication.java
+++ src/main/java/com/narbaniki/timescrimble/TimescrimbleApplication.java
@@ -1,4 +1,4 @@
-package com.nariki.timescrimble;
+package com.narbaniki.timescrimble;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
